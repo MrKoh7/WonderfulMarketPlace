a8f90e462c0883a4a03f1400a3fe71f1d0f1054e
KWY <weiyeekoh9@gmail.com>
2026-02-11 21:05:40 +0800

    initial project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { ENV } from './config/env';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on PORT: ${ENV.PORT}`);
+});
