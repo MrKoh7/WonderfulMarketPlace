0db1e6c3b75ca30c83d52f10b95788a66bf5a7e7
KWY <weiyeekoh9@gmail.com>
2026-02-14 18:02:14 +0800

    feat: enhance product and profile pages with comments functionality, including create and delete comments hooks

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,5 +1,11 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { createProduct, getAllProducts } from '../lib/api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getMyProducts,
+  getProductById,
+} from '../lib/api';
 
 export const useProducts = () => {
   const result = useQuery({ queryKey: ['products'], queryFn: getAllProducts });
@@ -8,4 +14,26 @@ export const useProducts = () => {
 
 export const useCreateProduct = () => {
   return useMutation({ mutationFn: createProduct });
+};
+
+export const useProduct = (id) => {
+  return useQuery({
+    queryKey: ['product', id],
+    queryFn: () => getProductById(id),
+    enabled: !!id, // double bang operator -> convert object to boolean
+  });
+};
+
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['myProducts'] });
+    },
+  });
+};
+
+export const useMyProducts = () => {
+  return useQuery({ queryKey: ['myProducts'], queryFn: getMyProducts });
 };
