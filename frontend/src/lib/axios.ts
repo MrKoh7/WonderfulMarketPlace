0220aa426acee721f626e956e3b25c5cffc0e392
KWY <weiyeekoh9@gmail.com>
2026-03-22 18:05:40 +0800

    feat: update axios baseURL to include API path

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true, // send cookies with req
 });
 
